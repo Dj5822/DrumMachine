f2313bd38b3d65ad427f3d38378f6d56090aa71b
dj5822 <dj5822@gmail.com>
2020-12-18 19:06:26 +1300

    Added display and drum-pads

--- src/Components/DrumMachine.js
+++ src/Components/DrumMachine.js
@@ -3,8 +3,17 @@ import React from 'react';
 class DrumMachine extends React.Component {
   render() {
     return (
-      <div>
-        <p>test</p>
+      <div id="drum-machine">
+        <div id="display"></div>
+        <button className="drum-pad" id="c-key">Q</button>
+        <button className="drum-pad" id="d-key">W</button>
+        <button className="drum-pad" id="e-key">E</button>
+        <button className="drum-pad" id="f-key">A</button>
+        <button className="drum-pad" id="g-key">S</button>
+        <button className="drum-pad" id="a-key">D</button>
+        <button className="drum-pad" id="b-key">Z</button>
+        <button className="drum-pad" id="high-c-key">X</button>
+        <button className="drum-pad" id="high-d-key">C</button>
       </div>
     );
   }
