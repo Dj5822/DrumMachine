d4276890c97d416a9652c83266db1ad2d0c5f2bc
dj5822 <dj5822@gmail.com>
2020-12-18 18:59:36 +1300

    Added Drum Machine Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DrumMachine from './Components/DrumMachine.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <DrumMachine />
     </div>
   );
 }
---
+++ src/Components/DrumMachine.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DrumMachine extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default DrumMachine;
