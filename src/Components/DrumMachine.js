0ab96568e2e172c16072a078690a112932cf772d
dj5822 <dj5822@gmail.com>
2020-12-20 22:47:10 +1300

    Set class as clip for the buttons.

--- src/Components/DrumMachine.js
+++ src/Components/DrumMachine.js
@@ -35,47 +35,47 @@ class DrumMachine extends React.Component {
       <div id="drum-machine" onKeyDown={this.handleKeyPress} tabIndex="0">
         <div id="display">{this.state.text}</div>
         <div id="drum-pad-container">
-          <button className="drum-pad" id="c-key" onClick={this.buttonClicked}>Q
+          <button className="drum-pad" id="c-key" class="clip" onClick={this.buttonClicked}>Q
             <audio id="Q">
               <source type="audio/mp3" src="../../Audio/c.mp3" />
             </audio>
           </button>
-          <button className="drum-pad" id="d-key" onClick={this.buttonClicked}>W
+          <button className="drum-pad" id="d-key" class="clip" onClick={this.buttonClicked}>W
             <audio id="W">
               <source type="audio/mp3" src="../../Audio/d.mp3" />
             </audio>
           </button>
-          <button className="drum-pad" id="e-key" onClick={this.buttonClicked}>E
+          <button className="drum-pad" id="e-key" class="clip" onClick={this.buttonClicked}>E
             <audio id="E">
               <source type="audio/mp3" src="../../Audio/e.mp3" />
             </audio>
           </button>
-          <button className="drum-pad" id="f-key" onClick={this.buttonClicked}>A
+          <button className="drum-pad" id="f-key" class="clip" onClick={this.buttonClicked}>A
             <audio id="A">
               <source type="audio/mp3" src="../../Audio/f.mp3" />
             </audio>
           </button>
-          <button className="drum-pad" id="g-key" onClick={this.buttonClicked}>S
+          <button className="drum-pad" id="g-key" class="clip" onClick={this.buttonClicked}>S
             <audio id="S">
               <source type="audio/mp3" src="../../Audio/g.mp3" />
             </audio>
           </button>
-          <button className="drum-pad" id="a-key" onClick={this.buttonClicked}>D
+          <button className="drum-pad" id="a-key" class="clip" onClick={this.buttonClicked}>D
             <audio id="D">
               <source type="audio/mp3" src="../../Audio/a.mp3" />
             </audio>
           </button>
-          <button className="drum-pad" id="b-key" onClick={this.buttonClicked}>Z
+          <button className="drum-pad" id="b-key" class="clip" onClick={this.buttonClicked}>Z
             <audio id="Z">
               <source type="audio/mp3" src="../../Audio/b.mp3" />
             </audio>
           </button>
-          <button className="drum-pad" id="c-sharp-key" onClick={this.buttonClicked}>X
+          <button className="drum-pad" id="c-sharp-key" class="clip" onClick={this.buttonClicked}>X
             <audio id="X">
               <source type="audio/mp3" src="../../Audio/cSharp.mp3" />
             </audio>
           </button>
-          <button className="drum-pad" id="d-sharp-key" onClick={this.buttonClicked}>C
+          <button className="drum-pad" id="d-sharp-key" class="clip" onClick={this.buttonClicked}>C
             <audio id="C">
               <source type="audio/mp3" src="../../Audio/dSharp.mp3" />
             </audio>
